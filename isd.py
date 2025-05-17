64c0b5fdca0856b4db66e05843ef9ce53dd8bcce
Cristobal Navarro <111240870+CristobalNavarroSchanentgen@users.noreply.github.com>
2025-05-18 00:06:00 +0200

    Add isd.py with Imperial Star Destroyer ASCII art

---
+++ isd.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# isd.py - Displays an Imperial Star Destroyer ASCII art
+
+def print_isd():
+    """
+    Prints an Imperial Star Destroyer ASCII art to the console
+    """
+    imperial_star_destroyer = r"""
+                                  /\
+                                 /  \
+                                /    \
+                               /      \
+                              /        \
+                             /          \
+                            /            \
+                           /              \
+                          /                \
+                         /                  \
+                        /                    \
+                       /                      \
+                      /                        \
+                     /                          \
+                    /                            \
+                   /                              \
+                  /                                \
+                 /                                  \
+                /                                    \
+               /                                      \
+              /                                        \
+             /                                          \
+            /                                            \
+           /                                              \
+          /                                                \
+         /                                                  \
+        /            [+]                    [+]              \
+       /             [+]                    [+]               \
+      /              [+]                    [+]                \
+     /               [+]                    [+]                 \
+    /                                                            \
+                 +--------------------------------+
+                 |     _____     +----+     _____  |
+                 |    /     \    |    |    /     \ |
+                 |    \_____/    +----+    \_____/ |
+                 +--------------------------------+
+  /                                                                \
+ /                                                                  \
+/                                                                    \
+----------------------------------------------------------------------
+             (========)           (========)           (========)
+             (========)           (========)           (========)
+             (========)           (========)           (========)
+"""
+    
+    print("\n\n")
+    print(imperial_star_destroyer)
+    print("\n")
+    print("                        IMPERIAL STAR DESTROYER")
+    print("                \"THE POWER OF THE EMPIRE UNLEASHED\"")
+    print("\n\n")
+
+if __name__ == "__main__":
+    print_isd()
